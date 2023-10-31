c6c15e085f863eed81deafa85bf4fc5d9eed0bcf
최은철 <wndqhrgkdlel@gmail.com>
2023-10-31 15:39:09 +0900

    이름 변경

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 // 정적 파일을 제공할 디렉토리 설정
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'ajax')));
 
 // 서버 시작
 app.listen(port, () => {
