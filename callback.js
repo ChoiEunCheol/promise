7ecf50ac47a6abb2773ed4e05f607b9bf27e3b69
최은철 <wndqhrgkdlel@gmail.com>
2023-11-01 15:18:57 +0900

    콜백 다시 수정

--- callback.js
+++ callback.js
@@ -1,29 +1,25 @@
-// XHR을 사용하여 JSON 데이터 가져오는 함수
+// Fetch 및 콜백 함수로 JSON 데이터 가져오는 함수
 function fetchDataWithCallback(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/1', true);
-  
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText);
-        callback(null, data); // 에러가 없을 경우 데이터 전달
-      } else {
-        callback(xhr.statusText, null); // 에러 발생 시 에러 메시지 전달
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('네트워크 오류');
       }
-    };
-  
-    xhr.onerror = function () {
-      callback('네트워크 오류', null); // 네트워크 오류 발생 시 에러 메시지 전달
-    };
-  
-    xhr.send();
+      return response.json();
+    })
+    .then(data => {
+      callback(null, data);
+    })
+    .catch(error => {
+      callback(error, null);
+    });
+}
+
+// fetchDataWithCallback 함수 사용 예제
+fetchDataWithCallback(function (error, data) {
+  if (error) {
+    console.error('에러:', error);
+  } else {
+    console.log('Fetch로 가져온 데이터:', data);
   }
-  
-  // fetchDataWithCallback 함수 사용 예제
-  fetchDataWithCallback(function (error, data) {
-    if (error) {
-      console.error('에러:', error);
-    } else {
-      console.log('XHR로 가져온 데이터:', data);
-    }
-  });
+});
