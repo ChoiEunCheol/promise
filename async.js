50cf8db3791021b9032a20e5108aface32b6c396
최은철 <wndqhrgkdlel@gmail.com>
2023-11-01 18:03:10 +0900

    async 디버깅 포인트

--- async.js
+++ async.js
@@ -2,6 +2,7 @@
 async function fetchDataWithAsyncAwait() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    // console.log(response.json());
     if (!response.ok) {
       throw new Error('네트워크 오류');
     }
